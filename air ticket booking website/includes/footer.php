a0cd87c770a6a7e7b8a25f44696f3d5c7a6aa3d9
Self-educate <86510066+Self-educate@users.noreply.github.com>
2021-06-27 13:47:55 +0530

    Add files via upload

---
+++ air ticket booking website/includes/footer.php
@@ -0,0 +1,32 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+
+
+			<ul>
+				<li><a class="facebook" href="#"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="#"><span>Twitter</span></a></li>
+				<li><a class="flickr" href="#"><span>Flickr</span></a></li>
+				<li><a class="googleplus" href="#"><span>Google+</span></a></li>
+				<li><a class="dribbble" href="#"><span>Dribbble</span></a></li>
+			</ul>
+					</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2020 UT&T . All Rights Reserved </p>
+		<div class="col-md-4 fotter-right" style="padding-left:70px" align="padding-left">
+			 <h3>Why buy from us?</h3><br/>
+              <div class="footer-list">
+			  <ul>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Payment Security</a></li>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Travel Guide</a></li>
+              <li style="color:#FFF"><a href="#" style="color:#FFF"><span></span> Social Responsibility</a></li>
+               </ul> </div></div>
+
+	</div>
+</div>
